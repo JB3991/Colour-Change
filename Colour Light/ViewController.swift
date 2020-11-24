b20038b72651ca64ff52066504c108c165784dd9
Jon Burnett <74561172+JB3991@users.noreply.github.com>
2020-11-24 17:14:48 +0000

    First Commit
    Main Commit

--- Colour Light/ViewController.swift
+++ Colour Light/ViewController.swift
@@ -1,19 +1,58 @@
 //
 //  ViewController.swift
-//  Colour Light
+//  Light
 //
 //  Created by Jonathan Burnett on 18/11/2020.
 //
 
 import UIKit
 
+var lightOn = true
+
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+        
     }
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    
+    func updateUI() {
+        
+        button.setTitle("Press Here", for: .normal)
+        
+        if lightOn {
+            view.backgroundColor = UIColor.getRandomColor()
+            _ = view.backgroundColor
+            button.tintColor = UIColor.getRandomColor()
 
+        } else {
+            view.backgroundColor = UIColor.getRandomColor()
+            _ = view.backgroundColor
+            button.tintColor = UIColor.getRandomColor()
+        }
+        
+        if view.backgroundColor == button.tintColor {
+            button.tintColor = UIColor.darkGray
+        }
+    }
 }
+    
+    extension UIColor {
+        
+        class func getRandomColor() -> UIColor {
+            let colors = [UIColor.red, UIColor.green, UIColor.blue, UIColor.white, UIColor.orange, UIColor.black, UIColor.purple, UIColor.yellow, UIColor.brown, UIColor.cyan]
+            let randomNumber = arc4random_uniform(UInt32(colors.count))
+            
+            return colors[Int(randomNumber)]
+        }
+    }
 
